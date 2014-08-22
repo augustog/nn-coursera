0041850a14a9fc955fb2c143a07dd557726318b0
Augusto Gesualdi <augusto.gesualdi@gmail.com>
2014-08-22 19:08:55 -0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""
+Simple neural network library with examples based on the concepts discussed in https://class.coursera.org/ml-006/
+"""
+
+__author__ = 'augusto'
---
+++ tests.py
@@ -0,0 +1,24 @@
+"""
+Test suite for the functions in the neural network package
+"""
+
+__author__ = 'augusto'
+
+import unittest
+import nn_define
+import nn_optimize
+
+
+class NNPredict (unittest.TestCase):
+    def test_make(self):
+        pass
+    def test_predict(self):
+        pass
+    def test_fit(self):
+        pass
+
+class NNOptimize (unittest.TestCase):
+    def test_cost_function(self):
+        pass
+    def test_optimize(self):
+        pass
